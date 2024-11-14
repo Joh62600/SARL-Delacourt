a7db8adc9a385a1966c7e397bde61c4847acfb89
Joh62600 <johann.plee@gmail.com>
2024-11-14 15:47:30 +0100

    GH PAGES V3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link,  } from 'react-router-dom';
 import Home from './pages/home';
 import MentionsLegales from './pages/mentions';
 import './App.css';
